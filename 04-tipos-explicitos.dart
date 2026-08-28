bcd9fd01467929823f4deda4e5fd0515f7fbebd0
duartedevs <110794962+duartedevs@users.noreply.github.com>
2026-08-28 20:43:06 -0300

    Create 04-tipos-explicitos.dart

---
+++ 04-tipos-explicitos.dart
@@ -0,0 +1,8 @@
+void main() {
+      String nome = 'Victor';
+      int idade = 27;
+      double altura = 1.82;
+      bool aprovado = true;
+  
+      print('$nome | $idade | $altura | $aprovado');
+}
