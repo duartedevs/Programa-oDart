a826bae9fa7f036c3cb1919c7da719cc16adf3e8
Gustavo Duarte <110794962+duartedevs@users.noreply.github.com>
2026-09-04 20:51:10 -0300

    Update metodo-list.dart

--- metodo-list.dart
+++ metodo-list.dart
@@ -6,5 +6,6 @@ void main() {
   final pares =numeros.where((n) => n.isEven).toList();
   print(pares);
   
-  final primeiroMaiorQue3 = numeros.firstWhere((n) =>n >3 ); 
+  final primeiroMaiorQue3 = numeros.firstWhere((n) =>n >3 );
+ print(primeiroMaiorQue3);
 }
