59635b2f6a76ad1f1292652a237941a3cf80be69
Gustavo Duarte <110794962+duartedevs@users.noreply.github.com>
2026-09-04 20:19:21 -0300

    Create list.dart

---
+++ list.dart
@@ -0,0 +1,9 @@
+void main() {
+  List<String> alunos = ['Adryan','Alan', 'Ariel', 'João'];
+  print(alunos);
+  alunos.add('Bruna');
+  print(alunos);
+  alunos.remove('Alan');
+  print(alunos);
+  print(alunos[0]);
+}
